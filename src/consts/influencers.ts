0c94ae9ae991e5705351bdf27a0029ce9274a91f
Juan Cruz Roldan <juancruz.roldan19@gmail.com>
2025-12-01 13:56:41 -0300

    Add influencers/ambassadors content

--- src/consts/influencers.ts
+++ src/consts/influencers.ts
@@ -10,16 +10,32 @@ export interface Influencer {
 export const INFLUENCERS: Influencer[] = [
   {
     id: 1,
-    name: "Influencer 1",
-    instagram: "@influencer1",
+    name: "Cata Cohan", // La fitness
+    instagram: "@catacohan",
     productSlug: "crema-mani-natural",
-    videoSrc: "https://www.w3schools.com/html/mov_bbb.mp4", // Placeholder
+    videoSrc: "/src/assets/videos/ambassadors/cata_cohan_IMG_7502.MOV",
   },
+  // {
+  //   id: 2,
+  //   name: "Delfina Rivas", // La bailarina de tango
+  //   instagram: "@delfinarivasok",
+  //   productSlug: "crema-mani-chocolate",
+  //   videoSrc:
+  //     "/src/assets/videos/ambassadors/delfi_rivas_WhatsApp Video 2025-11-12 at 16.24.41.mp4",
+  // },
+  // {
+  //   id: 3,
+  //   name: "Candelaria Diaz", // La rubia de pelo corto
+  //   instagram: "@candelariacontent",
+  //   productSlug: "crema-mani-natural",
+  //   videoSrc: "/src/assets/videos/ambassadors/cande_diaz_IMG_1995.MOV",
+  // },
   {
-    id: 2,
-    name: "Influencer 2",
-    instagram: "@influencer2",
-    productSlug: "crema-mani-chocolate",
-    videoSrc: "https://www.w3schools.com/html/movie.mp4", // Placeholder
+    id: 4,
+    name: "Florencia Gatti", // La nutri
+    instagram: "@thehealthynutrii",
+    productSlug: "crema-mani-natural",
+    videoSrc:
+      "/src/assets/videos/ambassadors/flor_nutri_WhatsApp Video 2025-11-19 at 16.10.33.mp4",
   },
 ];
