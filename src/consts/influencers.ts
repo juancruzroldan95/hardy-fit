fb23ee5909ca552df455f851fb30cb5f555acb45
Juan Cruz Roldan <juancruz.roldan19@gmail.com>
2025-12-04 11:43:03 -0300

    Fix video for dist

--- src/consts/influencers.ts
+++ src/consts/influencers.ts
@@ -13,7 +13,7 @@ export const INFLUENCERS: Influencer[] = [
     name: "Cata Cohan", // La fitness
     instagram: "@catacohan",
     productSlug: "crema-mani-natural",
-    videoSrc: "/src/assets/videos/ambassadors/cata_cohan_IMG_7502.MOV",
+    videoSrc: "/videos/cata_cohan_01.MOV",
   },
   // {
   //   id: 2,
@@ -21,21 +21,20 @@ export const INFLUENCERS: Influencer[] = [
   //   instagram: "@delfinarivasok",
   //   productSlug: "crema-mani-chocolate",
   //   videoSrc:
-  //     "/src/assets/videos/ambassadors/delfi_rivas_WhatsApp Video 2025-11-12 at 16.24.41.mp4",
+  //     "/videos/delfi_rivas_01.mp4",
   // },
   // {
   //   id: 3,
   //   name: "Candelaria Diaz", // La rubia de pelo corto
   //   instagram: "@candelariacontent",
   //   productSlug: "crema-mani-natural",
-  //   videoSrc: "/src/assets/videos/ambassadors/cande_diaz_IMG_1995.MOV",
+  //   videoSrc: "/videos/cande_diaz_01.MOV",
   // },
   {
     id: 4,
     name: "Florencia Gatti", // La nutri
     instagram: "@thehealthynutrii",
     productSlug: "crema-mani-natural",
-    videoSrc:
-      "/src/assets/videos/ambassadors/flor_nutri_WhatsApp Video 2025-11-19 at 16.10.33.mp4",
+    videoSrc: "/videos/flor_nutri_01.mp4",
   },
 ];
