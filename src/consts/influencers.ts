04af3a4ff1ffaee27efd583cd18d1ade2e0eedba
Juan Cruz Roldan <juancruz.roldan19@gmail.com>
2025-12-23 22:27:24 -0300

    Add new ambassadors and implement auto-play to videos

--- src/consts/influencers.ts
+++ src/consts/influencers.ts
@@ -2,7 +2,7 @@ export interface Influencer {
   id: number;
   name: string;
   instagram: string;
-  productSlug: string;
+  productSlug?: string;
   videoSrc: string;
   thumbnail?: string;
 }
@@ -12,29 +12,36 @@ export const INFLUENCERS: Influencer[] = [
     id: 1,
     name: "Cata Cohan", // La fitness
     instagram: "@catacohan",
-    productSlug: "crema-mani-natural",
     videoSrc: "/videos/cata_cohan_01.MOV",
   },
   // {
   //   id: 2,
   //   name: "Delfina Rivas", // La bailarina de tango
   //   instagram: "@delfinarivasok",
-  //   productSlug: "crema-mani-cookies",
-  //   videoSrc:
-  //     "/videos/delfi_rivas_01.mp4",
+  //   videoSrc: "/videos/delfi_rivas_01.mp4",
   // },
   // {
   //   id: 3,
   //   name: "Candelaria Diaz", // La rubia de pelo corto
   //   instagram: "@candelariacontent",
-  //   productSlug: "crema-mani-natural",
   //   videoSrc: "/videos/cande_diaz_01.MOV",
   // },
   {
     id: 4,
     name: "Florencia Gatti", // La nutri
     instagram: "@thehealthynutrii",
-    productSlug: "crema-mani-natural",
     videoSrc: "/videos/flor_nutri_01.mp4",
+  },
+  {
+    id: 5,
+    name: "Juana Lujan",
+    instagram: "@juanilujan",
+    videoSrc: "/videos/juana_lujan.mp4",
+  },
+  {
+    id: 5,
+    name: "Alex Sigler",
+    instagram: "@sigleralex.fitness",
+    videoSrc: "/videos/alex_sigler.mp4",
   },
 ];
